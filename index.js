f07cf45dcd929b2582b55a15e059aab6720e5efa
Rishabh Gist <damyrachit@gmail.com>
2022-10-24 17:37:29 +0530

    added express, dirname and fileURLToPath

---
+++ index.js
@@ -0,0 +1,8 @@
+// Imports
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
